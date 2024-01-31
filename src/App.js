1ef40ec59c02a17a49b6c4976a8bc672c9fa8b2c
Bhoomika <bhoomikahm18@gmail.com>
2024-01-31 11:53:37 +0530

    Routes config

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home/Home';
+import BookingCar from './pages/BookingCar/BookingCar';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
 
 function App() {
   return (
     <div className="App">
-      Hello world
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/bookingcar' element={<BookingCar />} />
+      </Routes>
     </div>
   );
 }
