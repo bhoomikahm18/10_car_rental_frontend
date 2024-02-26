38fd5760efaa22e77f79ca85db668e6c1dbb0014
Bhoomika <bhoomikahm18@gmail.com>
2024-02-26 12:28:32 +0530

    Installed redux thunk and designed the card

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,10 +2,11 @@ import React, { useEffect, useState } from 'react'
 // import Header from '../../components/Header/Header.jsx';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllCars } from '../../store/car_actions.js';
-import { Box } from '@mui/material';
 import Paper from '@mui/material/Paper';
 import { styled } from '@mui/material/styles';
 import { carActions } from '../../store/car_slice.js';
+import { ExpandMore } from '@mui/icons-material';
+import { Button, Card, CardActions, CardContent, Typography } from '@mui/material';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -19,11 +20,10 @@ function Home() {
   const cars = useSelector((state) => state.car.cars);
   const dispatch = useDispatch();
 
-  // const [carList, setCarList] = useState();
   console.log(cars);
 
   useEffect(() => {
-    dispatch(getAllCars());
+    dispatch(getAllCars())
   }, [dispatch]);
 
   // useEffect(() => {
@@ -34,13 +34,38 @@ function Home() {
   // })
 
   return (
-    <div>
-      <Box>
-        {cars.map(car => {
-          <div>{car.name}</div>
-        })}
-      </Box>
-    </div>
+    <>
+      <Card
+        sx={{
+          margin: 2,
+          width: 250,
+          height: 320,
+          borderRadius: 5,
+          ":hover": {
+            boxShadow: "10px 10px 20px #ccc",
+          },
+        }}
+      >
+        <img height={"50%"} width="100%" />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Car name
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <ExpandMore>
+            <Button
+              variant="outlined"
+              fullWidth
+              size="small"
+            >
+              Book Car
+            </Button>
+          </ExpandMore>
+
+        </CardActions>
+      </Card>
+    </>
   )
 }
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { applyMiddleware, configureStore } from "@reduxjs/toolkit";
 import carSlice from "./car_slice";
 import alertSlice from "./alert_slice";
+// import { thunk } from "redux-thunk";
 
 const store = configureStore({
     reducer: {
         car: carSlice.reducer,
         alert: alertSlice.reducer
-    }
+    },
+    // middleware: () => {
+    //     return applyMiddleware([thunk])
+    // }
 });
 
 export default store;
