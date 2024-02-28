e8ec3e9f7be1f4befecc28d9a9fad467410f2497
Bhoomika <bhoomikahm18@gmail.com>
2024-02-28 15:40:18 +0530

    Adding navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Home from './pages/Home/Home';
 import BookingCar from './pages/BookingCar/BookingCar';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
+import Navbar from './pages/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
