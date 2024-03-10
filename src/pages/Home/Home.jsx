236fdaf6f9b3b22bd5ee80cd73d4ab3a980304ea
Bhoomika <bhoomikahm18@gmail.com>
2024-03-10 20:45:55 +0530

    Designing expand more button

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,30 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 // import Header from '../../components/Header/Header.jsx';
-import '../../index.css';
+import './Home.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllCars } from '../../store/car_actions.js';
-import { ExpandMore } from '@mui/icons-material';
-import { Button, Card, CardActions, CardContent, Typography } from '@mui/material';
+import { Button, Card, CardActions, CardContent, CardMedia, Collapse, Typography } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import IconButton from '@mui/material/IconButton';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import styled from '@emotion/styled';
 
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  // transition: theme.transitions.create('transform', {
+  //   duration: theme.transitions.duration.shortest,
+  // }),
+}));
 
 function Home() {
   const cars = useSelector((state) => state.car.cars);
   const dispatch = useDispatch();
+  const [expanded, setExpanded] = React.useState(false);
 
   console.log(cars);
 
@@ -17,44 +32,50 @@ function Home() {
     dispatch(getAllCars())
   }, [dispatch]);
 
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
 
   return (
-    < div className='cars_home'>
+    < div className='cars_home' >
       {cars.map(car => {
         return <div >
-          <Card
-            sx={{
-              margin: 2,
-              width: 250,
-              height: 320,
-              borderRadius: 2,
-              ":hover": {
-                boxShadow: "10px 10px 20px #ccc",
-              },
-            }}
-          >
-            <img height={"65%"} width="100%" src={car.image} alt='car_image' />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
+          <Card sx={{
+            margin: 2,
+            maxWidth: 250,
+            // height: 320,
+            borderRadius: 2,
+            ":hover": {
+              boxShadow: "10px 10px 20px #ccc",
+            },
+          }}>
+            <CardMedia
+              component="img"
+              height="194"
+              image={car.image}
+              alt="Cars Pictures"
+            />
+            <CardActions disableSpacing>
+              <Typography paragraph>
                 {car.name}
-
               </Typography>
-            </CardContent>
-            <CardActions>
-              <ExpandMore>
-                <Typography>Capacity: {car.capacity}</Typography>
-                <Typography>Fuel Type: {car.Feultype}</Typography>
-                <Typography>{car.rentPerHour} Rent /hour</Typography>
-                <Button
-                  variant="outlined"
-                  fullWidth
-                  size="small"
-                >
-                  Book Car
-                </Button>
+              <ExpandMore
+                expand={expanded}
+                onClick={handleExpandClick}
+                aria-expanded={expanded}
+                aria-label="show more"
+              >
+                <ExpandMoreIcon />
               </ExpandMore>
-
             </CardActions>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+              <CardContent>
+
+                <Typography paragraph>Capacity: {car.capacity}</Typography>
+                <Typography paragraph>Fuel Type: {car.feultype}</Typography>
+                <Typography paragraph>{car.rentPerHour} Rent per hour</Typography>
+              </CardContent>
+            </Collapse>
           </Card>
         </div>
       })}
